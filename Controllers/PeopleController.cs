32b6bc6fbcdaea9434a004058f661b6dcbbc0085
Doc <SidRatGames@Protonmail.com>
2022-06-02 14:11:54 -0400

    3.04

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+namespace SwapiMVC.Controllers
+{
+    public class PeopleController : Controller
+    {
+       public IActionResult Index() 
+       {
+           return View ();
+       }
+    }
+}
